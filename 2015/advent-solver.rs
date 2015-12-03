0f363de42f6fe27dcc89c80aa16b322a3fda9ed4
Daniel O'Brien <github@danielobrien.net>
2015-12-03 16:25:39 +1030

    Improved results output slightly

--- 2015/advent-solver.rs
+++ 2015/advent-solver.rs
@@ -3,9 +3,9 @@ use std::fs::File;
 use std::io::Read;
 
 fn main() {
-  print_results(day01::solve(&get_input(1, "()())(("))); // 1, 5
-  print_results(day02::solve(&get_input(2, "2x3x4"))); // 58, 34
-  print_results(day03::solve(&get_input(3, "^v^v^v^v^v"))); //2, 11
+  print_results(1, day01::solve(&get_input(1, "()())(("))); // 1, 5
+  print_results(2, day02::solve(&get_input(2, "2x3x4"))); // 58, 34
+  print_results(3, day03::solve(&get_input(3, "^v^v^v^v^v"))); //2, 11
 }
 
 fn get_input(day: usize, or_else: &str) -> String {
@@ -18,14 +18,15 @@ fn get_input(day: usize, or_else: &str) -> String {
   input
 }
 
-fn print_results<T, E>(results: Vec<Result<T, E>>)
+fn print_results<T, E>(day: usize, results: Vec<Result<T, E>>)
 where T: Display,
       E: Display,
 {
+  println!("Day {}", day);
   for pair in (1..).zip(results) {
     match pair {
-      (i, Ok(a)) => println!("Part {}: {}", i, a);
-      (i, Err(e)) => println!("Could not solve Part {} due to error: {}", i, e);
+      (i, Ok(a)) => println!("  Part {}: {}", i, a);
+      (i, Err(e)) => println!("  Could not solve Part {} due to error: {}", i, e);
     }
   }
 }
